2bec3c86ac75d8ed2906a9c8d54aa74d05ff5bc6
nullgemm <nullgemm@mailbox.org>
2021-01-31 15:51:14 +0100

    fix remote editing script

--- make/git_remotes.sh
+++ make/git_remotes.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-git remote add gitea ssh://git@git.nullgemm.fr:2999/nullgemm/$(NAME).git
-git remote add github git@github.com:nullgemm/$(NAME).git
+git remote add gitea ssh://git@git.nullgemm.fr:2999/nullgemm/globox.git
+git remote add github git@github.com:nullgemm/globox.git
 git remote remove origin
