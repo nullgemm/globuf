c92011e6a96fb8dbbe8472930270f7d0a2148c25
nullgemm <nullgemm@mailbox.org>
2021-07-04 19:20:46 +0200

    automatically set the executable bit on dylibs and dlls

--- make/lib/release/release_zip.sh
+++ make/lib/release/release_zip.sh
@@ -8,4 +8,6 @@ tag=$(git tag | tail -n 1)
 release=globox_bin_$tag
 
 cp license.md "$release/lib/"
+chmod +x $release/lib/globox/macos/*.dylib
+chmod +x $release/lib/globox/windows/*.dll
 zip -r "$release.zip" "$release"
