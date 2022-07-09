599a5db2a0fa3d0daceeed6dabc8fa3c3e11f3ec
nullgemm <nullgemm@mailbox.org>
2022-07-09 13:50:17 +0200

    add the internal headers containing backend function pointers prototypes

---
+++ src/x11/x11_egl.h
@@ -0,0 +1,73 @@
+#ifndef H_GLOBOX_INTERNAL_X11_EGL
+#define H_GLOBOX_INTERNAL_X11_EGL
+
+#include "globox.h"
+
+void globox_x11_egl_init(
+	struct globox* context);
+
+void globox_x11_egl_clean(
+	struct globox* context);
+
+void globox_x11_egl_window_create(
+	struct globox* context);
+
+void globox_x11_egl_window_destroy(
+	struct globox* context);
+
+void globox_x11_egl_window_start(
+	struct globox* context);
+
+void globox_x11_egl_window_stop(
+	struct globox* context);
+
+void globox_x11_egl_init_features(
+	struct globox* context,
+	struct globox_config_features* config);
+
+void globox_x11_egl_init_events(
+	struct globox* context,
+	struct globox_config_events* config);
+
+void globox_x11_egl_set_interaction(
+	struct globox* context,
+	struct globox_feature_interaction* config);
+
+void globox_x11_egl_set_state(
+	struct globox* context,
+	struct globox_feature_state* config);
+
+void globox_x11_egl_set_title(
+	struct globox* context,
+	struct globox_feature_title* config);
+
+void globox_x11_egl_set_icon(
+	struct globox* context,
+	struct globox_feature_icon* config);
+
+void globox_x11_egl_set_init_size(
+	struct globox* context,
+	struct globox_feature_init_size* config);
+
+void globox_x11_egl_set_init_pos(
+	struct globox* context,
+	struct globox_feature_init_pos* config);
+
+void globox_x11_egl_set_frame(
+	struct globox* context,
+	struct globox_feature_frame* config);
+
+void globox_x11_egl_set_background(
+	struct globox* context,
+	struct globox_feature_background* config);
+
+void globox_x11_egl_set_vsync_callback(
+	struct globox* context,
+	struct globox_feature_vsync_callback* config);
+
+// for this backend, `data` is unused
+void globox_x11_egl_update_content(
+	struct globox* context,
+	void* data);
+
+#endif
---
+++ src/x11/x11_software.h
@@ -0,0 +1,73 @@
+#ifndef H_GLOBOX_INTERNAL_X11_SOFTWARE
+#define H_GLOBOX_INTERNAL_X11_SOFTWARE
+
+#include "globox.h"
+
+void globox_x11_software_init(
+	struct globox* context);
+
+void globox_x11_software_clean(
+	struct globox* context);
+
+void globox_x11_software_window_create(
+	struct globox* context);
+
+void globox_x11_software_window_destroy(
+	struct globox* context);
+
+void globox_x11_software_window_start(
+	struct globox* context);
+
+void globox_x11_software_window_stop(
+	struct globox* context);
+
+void globox_x11_software_init_features(
+	struct globox* context,
+	struct globox_config_features* config);
+
+void globox_x11_software_init_events(
+	struct globox* context,
+	struct globox_config_events* config);
+
+void globox_x11_software_set_interaction(
+	struct globox* context,
+	struct globox_feature_interaction* config);
+
+void globox_x11_software_set_state(
+	struct globox* context,
+	struct globox_feature_state* config);
+
+void globox_x11_software_set_title(
+	struct globox* context,
+	struct globox_feature_title* config);
+
+void globox_x11_software_set_icon(
+	struct globox* context,
+	struct globox_feature_icon* config);
+
+void globox_x11_software_set_init_size(
+	struct globox* context,
+	struct globox_feature_init_size* config);
+
+void globox_x11_software_set_init_pos(
+	struct globox* context,
+	struct globox_feature_init_pos* config);
+
+void globox_x11_software_set_frame(
+	struct globox* context,
+	struct globox_feature_frame* config);
+
+void globox_x11_software_set_background(
+	struct globox* context,
+	struct globox_feature_background* config);
+
+void globox_x11_software_set_vsync_callback(
+	struct globox* context,
+	struct globox_feature_vsync_callback* config);
+
+// for this backend, `data` is of type `struct globox_update_software*`
+void globox_x11_software_update_content(
+	struct globox* context,
+	void* data);
+
+#endif
