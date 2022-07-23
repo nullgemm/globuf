14fdc806440f8e71e5ac24619b0f65e38e71d499
nullgemm <nullgemm@mailbox.org>
2022-07-23 15:55:16 +0200

    introduce globox common elf ninja generator

---
+++ make/lib/elf.sh
@@ -0,0 +1,237 @@
+#!/bin/bash
+
+# get into the script's folder
+cd "$(dirname "$0")" || exit
+cd ../..
+
+# params
+build=$1
+
+echo "syntax reminder: $0 <build type>"
+echo "build types: development, release, sanitized"
+
+# utilitary variables
+tag=$(git tag --sort v:refname | tail -n 1)
+output="make/output"
+
+# ninja file variables
+folder_ninja="build"
+folder_objects="\$builddir/obj"
+folder_globox="globox_bin_$tag"
+folder_library="\$folder_globox/lib/globox"
+folder_include="\$folder_globox/include"
+name="globox"
+cc="gcc"
+ar="ar"
+
+# compiler flags
+flags+=("-std=c99" "-pedantic")
+flags+=("-Wall" "-Wextra" "-Werror=vla" "-Werror")
+flags+=("-Wformat")
+flags+=("-Wformat-security")
+flags+=("-Wno-address-of-packed-member")
+flags+=("-Wno-unused-parameter")
+flags+=("-Isrc")
+flags+=("-Isrc/include")
+flags+=("-fPIC")
+
+#defines+=("-DGLOBOX_ERROR_ABORT")
+#defines+=("-DGLOBOX_ERROR_SKIP")
+defines+=("-DGLOBOX_ERROR_LOG_THROW")
+
+# customize depending on the chosen build type
+if [ -z "$build" ]; then
+	build=development
+fi
+
+case $build in
+	development)
+flags+=("-g")
+defines+=("-DGLOBOX_ERROR_LOG_BASIC")
+defines+=("-DGLOBOX_ERROR_LOG_DEBUG")
+	;;
+
+	release)
+flags+=("-D_FORTIFY_SOURCE=2")
+flags+=("-fstack-protector-strong")
+flags+=("-fPIE")
+flags+=("-fPIC")
+flags+=("-O2")
+	;;
+
+	sanitized_memory)
+flags+=("-g")
+flags+=("-O1")
+flags+=("-fno-omit-frame-pointer")
+flags+=("-fno-optimize-sibling-calls")
+
+flags+=("-fsanitize=leak")
+flags+=("-fsanitize-recover=all")
+	;;
+
+	sanitized_undefined)
+flags+=("-g")
+flags+=("-O1")
+flags+=("-fno-omit-frame-pointer")
+flags+=("-fno-optimize-sibling-calls")
+
+flags+=("-fsanitize=undefined")
+flags+=("-fsanitize-recover=all")
+	;;
+
+	sanitized_address)
+flags+=("-g")
+flags+=("-O1")
+flags+=("-fno-omit-frame-pointer")
+flags+=("-fno-optimize-sibling-calls")
+
+flags+=("-fsanitize=address")
+flags+=("-fsanitize-address-use-after-scope")
+flags+=("-fsanitize-recover=all")
+	;;
+
+	sanitized_thread)
+flags+=("-g")
+flags+=("-O1")
+flags+=("-fno-omit-frame-pointer")
+flags+=("-fno-optimize-sibling-calls")
+
+flags+=("-fsanitize=thread")
+flags+=("-fsanitize-recover=all")
+	;;
+
+	*)
+echo "invalid build type"
+exit 1
+	;;
+esac
+
+# common globox lib for elf executables
+ninja_file=lib_elf.ninja
+src+=("src/common/globox.c")
+src+=("src/common/globox_error.c")
+
+# default target
+default+=("\$folder_library/\$name.a")
+
+# ninja start
+mkdir -p "$output"
+
+{ \
+echo "# vars"; \
+echo "builddir = $folder_ninja"; \
+echo "folder_objects = $folder_objects"; \
+echo "folder_globox = $folder_globox"; \
+echo "folder_library = $folder_library"; \
+echo "folder_include = $folder_include"; \
+echo "name = $name"; \
+echo "cc = $cc"; \
+echo "ar = $ar"; \
+echo ""; \
+} > "$output/$ninja_file"
+
+# ninja flags
+echo "# flags" >> "$output/$ninja_file"
+
+echo -n "flags =" >> "$output/$ninja_file"
+for flag in "${flags[@]}"; do
+	echo -ne " \$\n$flag" >> "$output/$ninja_file"
+done
+echo -e "\n" >> "$output/$ninja_file"
+
+echo -ne "defines =" >> "$output/$ninja_file"
+for define in "${defines[@]}"; do
+	echo -ne " \$\n$define" >> "$output/$ninja_file"
+done
+echo -e "\n" >> "$output/$ninja_file"
+
+# ninja rules
+{ \
+echo "# rules"; \
+echo "rule ar"; \
+echo "    command = \$ar rcs \$out \$in"; \
+echo "    description = ar \$out"; \
+echo ""; \
+} >> "$output/$ninja_file"
+
+{ \
+echo "rule cc"; \
+echo "    deps = $cc"; \
+echo "    depfile = \$out.d"; \
+echo "    command = \$cc \$flags \$defines -MMD -MF \$out.d -c \$in -o \$out"; \
+echo "    description = cc \$out"; \
+echo ""; \
+} >> "$output/$ninja_file"
+
+{ \
+echo "rule cp"; \
+echo "    command = cp \$in \$out"; \
+echo ""; \
+} >> "$output/$ninja_file"
+
+{ \
+echo "rule clean"; \
+echo "    command = make/scripts/clean.sh"; \
+echo "    description = cleaning repo"; \
+echo ""; \
+} >> "$output/$ninja_file"
+
+{ \
+echo "rule generator"; \
+echo "    command = make/lib/elf.sh $build"; \
+echo "    description = re-generating the ninja build file"; \
+echo ""; \
+} >> "$output/$ninja_file"
+
+# ninja targets
+## copy headers
+{ \
+echo "# copy headers"; \
+echo "build \$folder_headers/globox.h: \$"; \
+echo "cp src/include/globox.h"; \
+echo "build \$folder_headers/globox_egl.h: \$"; \
+echo "cp src/include/globox_egl.h"; \
+echo "build \$folder_headers/globox_vulkan.h: \$"; \
+echo "cp src/include/globox_vulkan.h"; \
+echo "build \$folder_headers/globox_software.h: \$"; \
+echo "cp src/include/globox_software.h"; \
+echo ""; \
+} >> "$output/$ninja_file"
+
+{ \
+echo "build headers: phony \$"; \
+echo "\$folder_headers/globox.h \$"; \
+echo "\$folder_headers/globox_egl.h \$"; \
+echo "\$folder_headers/globox_vulkan.h \$"; \
+echo "\$folder_headers/globox_software.h"; \
+echo ""; \
+} >> "$output/$ninja_file"
+
+## compile sources
+echo "# compile sources" >> "$output/$ninja_file"
+for file in "${src[@]}"; do
+	folder=$(dirname "$file")
+	filename=$(basename "$file" .c)
+	obj+=("\$folder_objects/$folder/$filename.o")
+	{ \
+	echo "build \$folder_objects/$folder/$filename.o: \$"; \
+	echo "cc $file"; \
+	echo ""; \
+	} >> "$output/$ninja_file"
+done
+
+## main targets
+echo "# archive objects" >> "$output/$ninja_file"
+echo -n "build \$folder_library/\$name.a: ar" >> "$output/$ninja_file"
+for file in "${obj[@]}"; do
+	echo -ne " \$\n$file" >> "$output/$ninja_file"
+done
+echo -e "\n" >> "$output/$ninja_file"
+
+## special targets
+{ \
+echo "# run special targets"; \
+echo "build regen: generator"; \
+echo "build clean: clean"; \
+echo "default" "${default[@]}"; \
+} >> "$output/$ninja_file"
