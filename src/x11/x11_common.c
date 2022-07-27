439c63bd1f9be2243d50e86aa47cfa18cc3f270c
nullgemm <nullgemm@mailbox.org>
2022-07-27 22:57:51 +0200

    fix compilation errors

--- src/x11/x11_common.c
+++ src/x11/x11_common.c
@@ -253,39 +253,51 @@ void globox_x11_common_window_create(
 	if ((context->feature_state != NULL)
 		&& (features[GLOBOX_FEATURE_STATE] != NULL))
 	{
-		*(context->feature_state) = *features[GLOBOX_FEATURE_STATE];
+		*(context->feature_state) =
+			*((struct globox_feature_state*)
+				features[GLOBOX_FEATURE_STATE]);
 	}
 
 	if ((context->feature_title != NULL)
 		&& (features[GLOBOX_FEATURE_TITLE] != NULL))
 	{
-		*(context->feature_title) = *features[GLOBOX_FEATURE_TITLE];
+		*(context->feature_title) =
+			*((struct globox_feature_title*)
+				features[GLOBOX_FEATURE_TITLE]);
 	}
 
 	if ((context->feature_icon != NULL)
 		&& (features[GLOBOX_FEATURE_ICON] != NULL))
 	{
-		*(context->feature_icon) = *features[GLOBOX_FEATURE_ICON];
+		*(context->feature_icon) =
+			*((struct globox_feature_icon*)
+				features[GLOBOX_FEATURE_ICON]);
 	}
 
 	// handled directly in xcb's window creation code
 	if ((context->feature_size!= NULL)
 		&& (features[GLOBOX_FEATURE_SIZE] != NULL))
 	{
-		*(context->feature_size) = *features[GLOBOX_FEATURE_SIZE];
+		*(context->feature_size) =
+			*((struct globox_feature_size*)
+				features[GLOBOX_FEATURE_SIZE]);
 	}
 
 	// handled directly in xcb's window creation code
 	if ((context->feature_pos != NULL)
 		&& (features[GLOBOX_FEATURE_POS] != NULL))
 	{
-		*(context->feature_pos) = *features[GLOBOX_FEATURE_POS];
+		*(context->feature_pos) =
+			*((struct globox_feature_pos*)
+				features[GLOBOX_FEATURE_POS]);
 	}
 
 	if ((context->feature_frame != NULL)
 		&& (features[GLOBOX_FEATURE_FRAME] != NULL))
 	{
-		*(context->feature_frame) = *features[GLOBOX_FEATURE_FRAME];
+		*(context->feature_frame) =
+			*((struct globox_feature_frame*)
+				features[GLOBOX_FEATURE_FRAME]);
 	}
 
 	// handled directly in xcb's window creation code for transparency,
@@ -293,17 +305,22 @@ void globox_x11_common_window_create(
 	if ((context->feature_background != NULL)
 		&& (features[GLOBOX_FEATURE_BACKGROUND] != NULL))
 	{
-		*(context->feature_background) = *features[GLOBOX_FEATURE_BACKGROUND];
+		*(context->feature_background) =
+			*((struct globox_feature_background*)
+				features[GLOBOX_FEATURE_BACKGROUND]);
 	}
 
 	if ((context->feature_vsync_callback != NULL)
 		&& (features[GLOBOX_FEATURE_VSYNC_CALLBACK] != NULL))
 	{
-		*(context->feature_vsync_callback) = *features[GLOBOX_FEATURE_VSYNC_CALLBACK];
+		*(context->feature_vsync_callback) =
+			*((struct globox_feature_vsync_callback*)
+				features[GLOBOX_FEATURE_VSYNC_CALLBACK]);
 	}
 
 	// prepare window attributes
-	if (background->background != GLOBOX_BACKGROUND_OPAQUE)
+	if ((context->feature_background != NULL)
+		&& (context->feature_background->background != GLOBOX_BACKGROUND_OPAQUE))
 	{
 		platform->attr_mask =
 			XCB_CW_BORDER_PIXEL
@@ -340,8 +357,8 @@ void globox_x11_common_window_create(
 			platform->root_win,
 			context->feature_pos->x,
 			context->feature_pos->y,
-			size->width,
-			size->height,
+			context->feature_size->width,
+			context->feature_size->height,
 			0,
 			XCB_WINDOW_CLASS_INPUT_OUTPUT,
 			platform->visual_id,
