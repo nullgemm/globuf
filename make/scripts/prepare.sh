c833b9e0c52d953dc5fc884d8a9fc7935585f6d2
nullgemm <nullgemm@mailbox.org>
2022-10-19 07:50:57 +0200

    update download scripts

---
+++ make/scripts/prepare.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# get in the right folder
+path="$(pwd)/$0"
+folder=$(dirname "$path")
+cd "$folder" || exit
+
+# get libs
+./cursoryx_get.sh
+
+# get dependencies
+./egl_get.sh
+
+# get local tools
+./infer_get.sh
