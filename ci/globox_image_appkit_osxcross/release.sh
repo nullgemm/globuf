b84f92cf790f72dbf6f719df67af81582f9a482f
nullgemm <nullgemm@mailbox.org>
2023-06-02 20:33:00 +0200

    add vulkan to ci system

--- ci/globox_image_appkit_osxcross/release.sh
+++ ci/globox_image_appkit_osxcross/release.sh
@@ -4,6 +4,7 @@ echo "please clean docker"
 
 sudo rm -rf globox_bin_v* log
 sudo docker rm globox_container_appkit_osxcross_software
+sudo docker rm globox_container_appkit_osxcross_vulkan
 sudo docker rm globox_container_appkit_osxcross_egl
 sudo docker rmi globox_image_appkit_osxcross
 sudo docker rmi alpine:edge
@@ -11,9 +12,11 @@ sudo docker rmi alpine:edge
 sudo ./build.sh
 
 sudo ./run.sh /scripts/build_appkit.sh release appkit software none osxcross
+sudo ./run.sh /scripts/build_appkit.sh release appkit vulkan none osxcross
 sudo ./run.sh /scripts/build_appkit.sh release appkit egl none osxcross
 
 sudo ./artifact.sh software
+sudo ./artifact.sh vulkan
 sudo ./artifact.sh egl
 
 sudo chown -R $(id -un):$(id -gn) globox_bin_v*
--- ci/globox_scripts_appkit_native/build_vulkan.sh
+++ ci/globox_scripts_appkit_native/build_vulkan.sh
@@ -9,13 +9,5 @@ git checkout appkit_angle
 # test build
 export PATH=/scripts/sdk/target/bin/:/bin:/sbin:/usr/bin:/usr/sbin
 export LD_LIBRARY_PATH=/scripts/sdk/target/lib/
-
-if [ "$3" == "egl" ]; then
-./make/scripts/angle_release_get.sh
-fi
-
-if [ "$3" == "vulkan" ]; then
 ./make/scripts/moltenvk_release_get.sh
-fi
-
-./make/scripts/build.sh "$@"
+./make/scripts/build.sh release appkit vulkan none native
