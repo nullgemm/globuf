60ff4ef7e3b844f478474b8445ad972b57661075
nullgemm <nullgemm@mailbox.org>
2023-06-02 19:56:34 +0200

    add native concourse runner macos scripts

---
+++ ci/globox_scripts_appkit_native/build_egl.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+git clone https://github.com/nullgemm/globox.git
+cd ./globox || exit
+
+# TODO remove
+git checkout appkit_angle
+
+# test build
+export PATH=/scripts/sdk/target/bin/:/bin:/sbin:/usr/bin:/usr/sbin
+export LD_LIBRARY_PATH=/scripts/sdk/target/lib/
+./make/scripts/angle_release_get.sh
+./make/scripts/build.sh release appkit egl none native
---
+++ ci/globox_scripts_appkit_native/build_software.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+git clone https://github.com/nullgemm/globox.git
+cd ./globox || exit
+
+# TODO remove
+git checkout appkit_angle
+
+# test build
+export PATH=/scripts/sdk/target/bin/:/bin:/sbin:/usr/bin:/usr/sbin
+export LD_LIBRARY_PATH=/scripts/sdk/target/lib/
+./make/scripts/build.sh release appkit software none native
