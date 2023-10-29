bbc45966b462b1f2adadcf041d1a01659fcc2c11
nullgemm <nullgemm@mailbox.org>
2023-10-29 18:29:13 +0100

    remove temporary branch checkout in ci scripts

--- ci/globox_image_wayland_gcc/scripts/build_wayland.sh
+++ ci/globox_image_wayland_gcc/scripts/build_wayland.sh
@@ -3,9 +3,6 @@
 git clone https://github.com/nullgemm/globox.git
 cd ./globox || exit
 
-# TODO remove
-git checkout next
-
 if [ "$4" == "complex" ]; then
 ./make/scripts/cursoryx_get.sh
 ./make/scripts/dpishit_get.sh
--- ci/globox_scripts_appkit_native/build_vulkan.sh
+++ ci/globox_scripts_appkit_native/build_vulkan.sh
@@ -3,9 +3,6 @@
 git clone https://github.com/nullgemm/globox.git
 cd ./globox || exit
 
-# TODO remove
-git checkout next
-
 # test build
 export PATH=/scripts/sdk/target/bin/:/bin:/sbin:/usr/bin:/usr/sbin
 export LD_LIBRARY_PATH=/scripts/sdk/target/lib/
